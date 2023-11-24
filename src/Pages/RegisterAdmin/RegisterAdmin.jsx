cd6abe277d19e03b7dac9c5235b885b3f9ba313b
shathi <shathi@gmail.com>
2023-11-25 03:51:00 +0600

    design admin register page

--- src/Pages/RegisterAdmin/RegisterAdmin.jsx
+++ src/Pages/RegisterAdmin/RegisterAdmin.jsx
@@ -1,31 +1,52 @@
 
 const RegisterAdmin = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+        <div className="hero min-h-screen bg-SecondariColor w-11/12 mx-auto m-6 rounded-lg">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center h-full lg:text-left w-6/12 rounded-lg">
+                    <img src="https://previews.123rf.com/images/naum100/naum1001610/naum100161000080/64589806-man-working-on-laptop-computer-businessman-with-idea-and-gears-cartoon-vector-illustration.jpg" className="h-full w-full rounded-lg" alt="" />
                 </div>
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card shrink-0 w-6/12">
                     <form className="card-body">
+                        <h1 className="text-center text-2xl font-bold uppercase text-white">Register As Admin</h1>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text text-white">Full Name</span>
+                            </label>
+                            <input type="text" placeholder="Full Name" name="fullName" className="input input-bordered" required />
+                            <label className="label">
+                                <span className="label-text text-white">Company Name</span>
+                            </label>
+                            <input type="text" placeholder="Company Name" name="companyName" className="input input-bordered" required />
+                            <label className="label">
+                                <span className="label-text text-white">Company Logo URL</span>
+                            </label>
+                            <input type="text" placeholder="Company Logo URL" name="companyLogo" className="input input-bordered" required />
+                            <label className="label">
+                                <span className="label-text text-white">Email</span>
                             </label>
                             <input type="email" placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text text-white">Password</span>
                             </label>
                             <input type="password" placeholder="password" className="input input-bordered" required />
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <span className="label-text text-white">Date Of Birth</span>
                             </label>
+                            <input type="date" name="dob" className="input input-bordered" required />
+                            <label className="label">
+                                <span className="label-text text-white">Select a package</span>
+                            </label>
+                            <select className="input input-bordered" name="package" id="">
+                                <option value="5">5 Members for $5</option>
+                                <option value="8">10 Members for $8</option>
+                                <option value="18">20 Members for $15</option>
+                            </select>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="border-2 border-thirdColor text-white px-2 py-4 rounded-lg text-lg hover:bg-primaryColor duration-200">Sign Up</button>
                         </div>
                     </form>
                 </div>
