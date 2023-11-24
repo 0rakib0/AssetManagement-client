c6036e185a59b44366dbd709850afa0c9f8b564a
shathi <shathi@gmail.com>
2023-11-25 03:59:25 +0600

    complate employee registration page design

--- src/Pages/RegisterEmp/RegisterEmp.jsx
+++ src/Pages/RegisterEmp/RegisterEmp.jsx
@@ -5,24 +5,16 @@ const RegisterEmp = () => {
         <div className="hero min-h-screen bg-SecondariColor w-11/12 mx-auto m-6 rounded-lg">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="text-center h-full lg:text-left w-6/12 rounded-lg">
-                    <img src="https://previews.123rf.com/images/naum100/naum1001610/naum100161000080/64589806-man-working-on-laptop-computer-businessman-with-idea-and-gears-cartoon-vector-illustration.jpg" className="h-full w-full rounded-lg" alt="" />
+                    <img src="https://previews.123rf.com/images/microbagrandioza/microbagrandioza1810/microbagrandioza181000022/114029529-businessman-office-worker-worker-at-workplace-at-morning-working-day-concept-vector-flat-cartoon.jpg" className="h-full w-full rounded-lg" alt="" />
                 </div>
                 <div className="card shrink-0 w-6/12">
                     <form className="card-body">
-                        <h1 className="text-center text-2xl font-bold uppercase text-white">Register As Admin</h1>
+                        <h1 className="text-center text-2xl font-bold uppercase text-white">Register As Employee</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-white">Full Name</span>
                             </label>
                             <input type="text" placeholder="Full Name" name="fullName" className="input input-bordered" required />
-                            <label className="label">
-                                <span className="label-text text-white">Company Name</span>
-                            </label>
-                            <input type="text" placeholder="Company Name" name="companyName" className="input input-bordered" required />
-                            <label className="label">
-                                <span className="label-text text-white">Company Logo URL</span>
-                            </label>
-                            <input type="text" placeholder="Company Logo URL" name="companyLogo" className="input input-bordered" required />
                             <label className="label">
                                 <span className="label-text text-white">Email</span>
                             </label>
@@ -37,14 +29,6 @@ const RegisterEmp = () => {
                                 <span className="label-text text-white">Date Of Birth</span>
                             </label>
                             <input type="date" name="dob" className="input input-bordered" required />
-                            <label className="label">
-                                <span className="label-text text-white">Select a package</span>
-                            </label>
-                            <select className="input input-bordered" name="package" id="">
-                                <option value="5">5 Members for $5</option>
-                                <option value="8">10 Members for $8</option>
-                                <option value="18">20 Members for $15</option>
-                            </select>
                         </div>
                         <div className="form-control mt-6">
                             <button className="border-2 border-thirdColor text-white px-2 py-4 rounded-lg text-lg hover:bg-primaryColor duration-200">Sign Up</button>
