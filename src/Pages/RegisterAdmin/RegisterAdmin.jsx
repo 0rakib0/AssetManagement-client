c62b22a28dad56eeb2ae46f42bdae6d547ce2103
rakib <hassanrakibul926@gmail.com>
2023-11-25 19:30:53 +0600

    user register and create admin objects

--- src/Pages/RegisterAdmin/RegisterAdmin.jsx
+++ src/Pages/RegisterAdmin/RegisterAdmin.jsx
@@ -1,37 +1,85 @@
+import { updateProfile } from "firebase/auth";
+import useAuth from "../../Hooks/useAuth";
+import auth from "../../firebase.config";
 
 const RegisterAdmin = () => {
+
+    const {Register} = useAuth()
+
+    const handleSubmit = (event) =>{
+        event.preventDefault()
+        const form = event.target;
+        const full_name = form.fullName.value
+        const companyName = form.companyName.value
+        const comapnyLogo = form.companyLogo.value
+        const email = form.email.value
+        const password = form.password.value 
+        const dob = form.dob.value
+        const memberpackage = form.package.value
+
+        const AdminInfo = {
+            full_name, 
+            comapnyLogo, 
+            companyName, 
+            email, 
+            memberpackage, 
+            dob,
+            isAdmin: true
+        }
+        console.log(AdminInfo)
+        Register(email, password)
+        .then(result =>{
+            const user = result.user
+            updateProfile(auth.currentUser, {
+                displayName: full_name,
+                photoURL: comapnyLogo
+            })
+            .then(() =>{
+                console.log('Profile Successfully Update')
+            })
+            .catch(error =>{
+                console.log(error)
+            })
+            console.log(user)
+        })
+        .catch(error =>{
+            console.log(error.message)
+        })
+        
+    }
+
     return (
         <div className="hero min-h-screen bg-SecondariColor w-11/12 mx-auto m-6 rounded-lg">
             <div className="hero-content flex-col lg:flex-row">
                 <div className="text-center h-full lg:text-left w-6/12 rounded-lg">
                     <img src="https://previews.123rf.com/images/naum100/naum1001610/naum100161000080/64589806-man-working-on-laptop-computer-businessman-with-idea-and-gears-cartoon-vector-illustration.jpg" className="h-full w-full rounded-lg" alt="" />
                 </div>
-                <div className="card shrink-0 w-6/12">
-                    <form className="card-body">
+                <div className="card shrink-0 md:w-6/12">
+                    <form className="card-body" onSubmit={handleSubmit}>
                         <h1 className="text-center text-2xl font-bold uppercase text-white">Register As Admin</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-white">Full Name</span>
                             </label>
-                            <input type="text" placeholder="Full Name" name="fullName" className="input input-bordered border-thirdColor bg-primaryColor" required />
+                            <input type="text" placeholder="Full Name" name="fullName" className="input input-bordered border-thirdColor bg-primaryColor text-white" required />
                             <label className="label">
                                 <span className="label-text text-white">Company Name</span>
                             </label>
-                            <input type="text" placeholder="Company Name" name="companyName" className="input input-bordered border-thirdColor bg-primaryColor" required />
+                            <input type="text" placeholder="Company Name" name="companyName" className="input input-bordered border-thirdColor bg-primaryColor text-white" required />
                             <label className="label">
                                 <span className="label-text text-white">Company Logo URL</span>
                             </label>
-                            <input type="text" placeholder="Company Logo URL" name="companyLogo" className="input input-bordered border-thirdColor bg-primaryColor" required />
+                            <input type="text" placeholder="Company Logo URL" name="companyLogo" className="input input-bordered border-thirdColor bg-primaryColor text-white" required />
                             <label className="label">
                                 <span className="label-text text-white">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered border-thirdColor bg-primaryColor" required />
+                            <input type="email" placeholder="email" name="email" className="input input-bordered border-thirdColor bg-primaryColor text-white" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text text-white">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered border-thirdColor bg-primaryColor" required />
+                            <input type="password" placeholder="password" name="password" className="input input-bordered border-thirdColor bg-primaryColor text-white" required />
                             <label className="label">
                                 <span className="label-text text-white">Date Of Birth</span>
                             </label>
@@ -40,9 +88,9 @@ const RegisterAdmin = () => {
                                 <span className="label-text text-white">Select a package</span>
                             </label>
                             <select className="input input-bordered border-thirdColor bg-primaryColor text-white" name="package" id="">
-                                <option value="5">5 Members for $5</option>
-                                <option value="8">10 Members for $8</option>
-                                <option value="18">20 Members for $15</option>
+                                <option value="5member">5 Members for $5</option>
+                                <option value="10Member">10 Members for $8</option>
+                                <option value="20Member">20 Members for $15</option>
                             </select>
                         </div>
                         <div className="form-control mt-6">
--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,12 +1,45 @@
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
+import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import auth from "../../firebase.config";
 export const authContext = createContext()
 
 
 const AuthProvider = ({ children }) =>{
 
+    const [user, setUser] = useState('')
+    const [loading, setLoading] = useState(true)
+
+    const Register = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const Login = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const Logout = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    useEffect(() =>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => unSubscribe
+    },[])
+    
+
+
     const authInfo = {
-        user:'Rakibul Hasan'
+        user,
+        loading,
+        Register,
+        Login,
+        Logout,
     }
 
     return (
